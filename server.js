e0165d38a8370bfc22dba9efa205b7e650233b87
Sharpirate <bstoynov@yahoo.com>
2020-11-03 21:35:44 +0200

    created search endpoint

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const app = express();
+const axios = require('axios');
+const btoa = require('btoa');
+
+const CLIENT_ID = 'd9c317f2ee2f44d6b1a943f56999299e';
+const CLIENT_SECRET = 'ed6758fd58a144359a9482b1f1bada89';
+
+const port = 5000;
+let token;
+
+// Get token
+getToken(CLIENT_ID, CLIENT_SECRET)
+  .then(res => token = res);
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
+// Search endpoint
+app.get('/api/:query', async(req, res) => {
+  const artists = await getArtists(req.params.query, token);
+
+  if (artists.length > 0)
+    res.send(artists);
+})
+
+async function getToken(clientId, clientSecret) {
+  const config = {
+    method: 'post',
+    url: 'https://accounts.spotify.com/api/token',
+    headers: {
+      'Authorization': 'Basic ' + btoa(`${clientId}:${clientSecret}`),
+      'content-type': 'application/x-www-form-urlencoded'
+    },
+    data: 'grant_type=client_credentials'
+  };
+
+  const res = await axios(config);
+  
+  return res.data.access_token;
+}
+
+async function getArtists(query, token, limit = 3, offset = 0) {
+  const config = {
+    method: 'get',
+    url: `https://api.spotify.com/v1/search?q=${query}&type=artist&limit=${limit}&offset=${offset}`,
+    headers: {
+      'Authorization': `Bearer ${token}`,
+      'Content-Type': 'application/json'
+    }
+  };
+
+  const res = await axios(config);
+
+  return res.data.artists.items;
+}
