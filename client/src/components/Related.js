3522ca510e79f7219fd216d28161703ebcdd2010
Sharpirate <bstoynov@yahoo.com>
2020-11-07 11:35:28 +0200

    redux progress

---
+++ client/src/components/Related.js
@@ -0,0 +1,13 @@
+import Section from './Section';
+import Records from './Records';
+
+function Related() {
+
+  return (
+    <Section name="Related Artists">
+      <Records />
+    </Section>
+  );
+}
+
+export default Related;
