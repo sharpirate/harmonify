df8f1221c2cea57b4d48362982ff5f9619d35be9
Sharpirate <bstoynov@yahoo.com>
2020-11-18 22:32:10 +0200

    removed controlled input and debounced input

--- client/src/components/search/Search.js
+++ client/src/components/search/Search.js
@@ -6,40 +6,55 @@ import SearchField from './SearchField';
 const SearchWrap = styled.div``;
 
 function Search() {
-  const [fieldValue, setFieldValue] = useState('');
+  // const [fieldValue, setFieldValue] = useState('');
   const [results, setResults] = useState([]);
   const [resultsVisible, setResultsVisible] = useState(false);
 
-  function handleInput(e) {
-    setFieldValue(e.target.value);
+  function debounce(callback, wait) {
+    let timerId;
+    return (...args) => {
+      clearTimeout(timerId);
+      timerId = setTimeout(() => {
+        callback(...args);
+      }, wait);
+    };
   }
+
+  const handleInput = debounce((e) => {
+    fetch(`/api/search/${e.target.value}`)
+        .then(res => res.json())
+        .then(data => {
+          console.log(e.target.value)
+          console.log(data);
+          setResults(data);
+        });
+  }, 300)
 
   function handleFocus() {
     setResultsVisible(true);
   }
   
-  function handleBlur() {
+  function handleBlur(e) {
     // catch the click on a result before hiding them
     setTimeout(setResultsVisible.bind(this, false), 150);
-    setFieldValue('');
+    // e.taget.value = '';
   }
 
-  useEffect(() => {
-    if (fieldValue) {
-      fetch(`/api/search/${fieldValue}`)
-        .then(res => res.json())
-        .then(data => {
-          console.log(fieldValue)
-          console.log(data);
-          setResults(data);
-        });
-    }
-  }, [fieldValue]);
+  // useEffect(() => {
+  //   if (fieldValue) {
+  //     fetch(`/api/search/${fieldValue}`)
+  //       .then(res => res.json())
+  //       .then(data => {
+  //         console.log(fieldValue)
+  //         console.log(data);
+  //         setResults(data);
+  //       });
+  //   }
+  // }, [fieldValue]);
 
   return (
     <SearchWrap>
       <SearchField
-        value={fieldValue}
         handleInput={handleInput}
         handleFocus={handleFocus}
         handleBlur={handleBlur}
